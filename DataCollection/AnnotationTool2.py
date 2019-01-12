15eef6486783042febccec7a76d5abf369b39209
jyothsnapb <jpadmaku@purdue.edu>
2019-01-12 01:40:16 -0500

    Update AnnotationTool2.py

--- DataCollection/AnnotationTool2.py
+++ DataCollection/AnnotationTool2.py
@@ -9,22 +9,29 @@ import wx
 import wx.xrc
 import cv2
 
-###########################################################################
+##################################################################################################
 ## Class MyFrame
 ## Framework code generated with wxFormBuilder : http://www.wxformbuilder.org/
 ## You need python3 and wxPython, opencv-python module
 ## wxPython module: pip install wxPython
+## The size of display area can be modified by setting VideoDisplaySize parameter in main function
+## The surgeme list can also be easily editted.
+## Left and right arrow on keyboard can be used to show the last and next frame. 
+## Create annotation button is not necessary before 'write to file' action, but if you would like 
+## to edit the annotation, you can create it and then edit in the text area.
 ## Before closing the window, you'd better pause the video.
-###########################################################################
+##################################################################################################
 
 class AnnotationTool2 ( wx.Frame ):
-	def __init__( self, parent, video_path, write_path ):
-		wx.Frame.__init__ ( self, parent, id = wx.ID_ANY, title = wx.EmptyString, pos = wx.DefaultPosition, \
-                     size = wx.Size( 1050,665 ), style = wx.DEFAULT_FRAME_STYLE|wx.TAB_TRAVERSAL )
-		self.SetSizeHints( wx.DefaultSize, wx.DefaultSize )   
+    
+	def __init__( self, parent, video_path, write_path, VideoDisplaySize ):
+        # Define surgeme list and initial parameters
+		self.WindowWidth = VideoDisplaySize[0] + 440
+		self.WindowHeight = VideoDisplaySize[1] + 190
         
-        # Define choice lists and initial parameters
-		self.surgeme = ['S1', 'S2', 'S3', 'S4', 'S5', 'S6', 'S7', 'S8']
+		self.ImWidth = VideoDisplaySize[0]
+		self.ImHeight = VideoDisplaySize[1]
+		self.surgeme = ['S1', 'S2', 'S3', 'S4', 'S5', 'S6', 'S7']
 		self.s_f = ['Success', 'Fail']
 		self.FrameNumber = 0
 		self.FrameTime = 33
@@ -36,51 +43,54 @@ class AnnotationTool2 ( wx.Frame ):
 		self.OneAnnotation = ''
 		self.AnnotationList = []
         
+		wx.Frame.__init__ ( self, parent, id = wx.ID_ANY, title = wx.EmptyString, pos = wx.DefaultPosition, \
+                     size = wx.Size( self.WindowWidth, self.WindowHeight ), style = wx.DEFAULT_FRAME_STYLE|wx.TAB_TRAVERSAL )
+		self.SetSizeHints( wx.DefaultSize, wx.DefaultSize )   
+                
         #Define the framework
-		bSizer1 = wx.BoxSizer( wx.VERTICAL )
-		
+		bSizer1 = wx.BoxSizer( wx.VERTICAL )		
 		bSizer2 = wx.BoxSizer( wx.HORIZONTAL )
 		
+		bSizer2.SetMinSize( wx.Size( -1,20 ) ) 
 		self.m_load = wx.Button( self, wx.ID_ANY, u"Load", wx.DefaultPosition, wx.DefaultSize, 0 )
-		bSizer2.Add( self.m_load, 1, wx.ALL, 5 )
+		bSizer2.Add( self.m_load, 1, wx.ALL|wx.EXPAND, 5 )
 		
 		self.Inform_bar = wx.TextCtrl( self, wx.ID_ANY, u"Information is shown here", \
                                 wx.DefaultPosition, wx.DefaultSize, wx.TE_CENTRE|wx.TE_READONLY )
-		self.Inform_bar.SetBackgroundColour( wx.SystemSettings.GetColour( wx.SYS_COLOUR_MENU ) )
-		
+		self.Inform_bar.SetBackgroundColour( wx.SystemSettings.GetColour( wx.SYS_COLOUR_MENU ) )		
 		bSizer2.Add( self.Inform_bar, 3, wx.ALL|wx.ALIGN_CENTER_VERTICAL|wx.EXPAND, 5 )
-			
-		bSizer1.Add( bSizer2, 1, wx.EXPAND, 5 )
-		
-		sbSizer3 = wx.StaticBoxSizer( wx.StaticBox( self, wx.ID_ANY, u"PlayVideo" ), wx.VERTICAL )
-		
+				
+		bSizer1.Add( bSizer2, 0, wx.EXPAND, 5 )		
+		sbSizer3 = wx.StaticBoxSizer( wx.StaticBox( self, wx.ID_ANY, u"PlayVideo" ), wx.VERTICAL )		
 		bSizer4 = wx.BoxSizer( wx.HORIZONTAL )
 		
 		self.m_bitmap = wx.StaticBitmap( sbSizer3.GetStaticBox(), wx.ID_ANY, wx.NullBitmap, \
-                                  wx.DefaultPosition, wx.Size( 640,480 ), 0 )
+                                  wx.DefaultPosition, wx.Size(self.ImWidth,self.ImHeight ), 0 )
 		bSizer4.Add( self.m_bitmap, 0, wx.ALL, 5 )
 		
 		bSizer5 = wx.BoxSizer( wx.VERTICAL )
 		
-		self.m_toggleBtn2 = wx.ToggleButton( sbSizer3.GetStaticBox(), wx.ID_ANY, \
-                                      u"Choose Start Frame", wx.Point( -1,-1 ), wx.Size( 200,30 ), 0 )
-		bSizer5.Add( self.m_toggleBtn2, 0, wx.ALIGN_CENTER|wx.ALL, 5 )
+		bSizer5.SetMinSize( wx.Size( 210,-1 ) ) 
+		self.m_toggleBtn2 = wx.ToggleButton( sbSizer3.GetStaticBox(), wx.ID_ANY,\
+                                      u"Choose Start Frame", wx.Point( -1,-1 ), wx.Size( 180,-1 ), 0 )
+		self.m_toggleBtn2.SetValue( True ) 
+		bSizer5.Add( self.m_toggleBtn2, 1, wx.ALIGN_CENTER|wx.ALL, 5 )
 		
 		self.m_staticText = wx.StaticText( sbSizer3.GetStaticBox(), wx.ID_ANY, \
-                                    u"Choose Surgeme :", wx.DefaultPosition, wx.DefaultSize, 0 )
+                                    u"\nChoose Surgeme :", wx.DefaultPosition, wx.DefaultSize, 0 )
 		self.m_staticText.Wrap( -1 )
 		bSizer5.Add( self.m_staticText, 0, wx.ALL, 5 )
 		
 		m_listBoxChoices = self.surgeme
-		self.m_listBox = wx.ListBox( sbSizer3.GetStaticBox(), wx.ID_ANY, wx.DefaultPosition, wx.Size( 120,200 ),\
-                              m_listBoxChoices, wx.LB_EXTENDED|wx.LB_HSCROLL|wx.LB_NEEDED_SB|wx.LB_SINGLE )
-		self.m_listBox.SetFont( wx.Font( 14, 70, 90, 90, False, "Calibri" ) )
+		self.m_listBox = wx.ListBox( sbSizer3.GetStaticBox(), wx.ID_ANY, wx.DefaultPosition, \
+                              wx.Size( 120,-1 ), m_listBoxChoices, wx.LB_EXTENDED|wx.LB_HSCROLL|wx.LB_NEEDED_SB|wx.LB_SINGLE )
+		self.m_listBox.SetFont( wx.Font( 16, 70, 90, 90, False, "Century" ) )
 		self.m_listBox.SetBackgroundColour( wx.SystemSettings.GetColour( wx.SYS_COLOUR_BTNFACE ) )
 		
-		bSizer5.Add( self.m_listBox, 0, wx.ALL|wx.ALIGN_CENTER_HORIZONTAL, 5 )
+		bSizer5.Add( self.m_listBox, 4, wx.ALL|wx.ALIGN_CENTER_HORIZONTAL, 5 )
 		
 		self.m_staticText2 = wx.StaticText( sbSizer3.GetStaticBox(), wx.ID_ANY, \
-                                     u"Success or Fail :", wx.DefaultPosition, wx.DefaultSize, 0 )
+                                     u"\nSuccess or Fail :", wx.DefaultPosition, wx.DefaultSize, 0 )
 		self.m_staticText2.Wrap( -1 )
 		bSizer5.Add( self.m_staticText2, 0, wx.ALL, 5 )
 		
@@ -89,86 +99,73 @@ class AnnotationTool2 ( wx.Frame ):
 		self.m_choice.SetSelection( 0 )
 		bSizer5.Add( self.m_choice, 0, wx.ALIGN_CENTER_HORIZONTAL|wx.ALL, 5 )
 		
-		self.m_Anno = wx.Button( sbSizer3.GetStaticBox(), wx.ID_ANY, \
-                          u"Create Annotation :", wx.DefaultPosition, wx.Size( -1,30 ), 0 )
-		self.m_Anno.SetMinSize( wx.Size( -1,30 ) )
+		self.m_Anno = wx.Button( sbSizer3.GetStaticBox(), wx.ID_ANY, u"Create Annotation :", \
+                          wx.DefaultPosition, wx.Size( 180,-1 ), 0 )
+		bSizer5.Add( self.m_Anno, 1, wx.ALL|wx.ALIGN_CENTER_HORIZONTAL, 5 )
 		
-		bSizer5.Add( self.m_Anno, 0, wx.ALL, 5 )
-		
-		self.m_Annotext = wx.TextCtrl( sbSizer3.GetStaticBox(), wx.ID_ANY, wx.EmptyString, \
-                                wx.DefaultPosition, wx.Size( 180,-1 ), wx.TE_CENTRE|wx.TE_READONLY )
-		self.m_Annotext.SetFont( wx.Font( wx.NORMAL_FONT.GetPointSize(), 70, 90, 90, False, "Century" ) )
+		self.m_Annotext = wx.TextCtrl( sbSizer3.GetStaticBox(), wx.ID_ANY, \
+                                wx.EmptyString, wx.DefaultPosition, wx.Size( 180,-1 ), wx.TE_CENTRE)
+		self.m_Annotext.SetFont( wx.Font( 14, 70, 90, 90, False, "Century" ) )
 		self.m_Annotext.SetForegroundColour( wx.SystemSettings.GetColour( wx.SYS_COLOUR_BACKGROUND ) )
 		self.m_Annotext.SetBackgroundColour( wx.SystemSettings.GetColour( wx.SYS_COLOUR_MENU ) )
 		self.m_Annotext.SetMinSize( wx.Size( 180,-1 ) )
-		self.m_Annotext.SetMaxSize( wx.Size( 180,-1 ) )
-		
-		bSizer5.Add( self.m_Annotext, 0, wx.ALL|wx.ALIGN_CENTER_HORIZONTAL, 5 )
+		self.m_Annotext.SetMaxSize( wx.Size( 180,-1 ) )		
+		bSizer5.Add( self.m_Annotext, 1, wx.ALL|wx.ALIGN_CENTER_HORIZONTAL, 5 )
 		
 		self.m_Write = wx.Button( sbSizer3.GetStaticBox(), wx.ID_ANY, u"Write To File", wx.Point( -1,-1 ), wx.Size( 140,30 ), 0 )
 		self.m_Write.SetMinSize( wx.Size( 130,30 ) )
-		self.m_Write.SetMaxSize( wx.Size( 160,40 ) )
-		
+		self.m_Write.SetMaxSize( wx.Size( 160,40 ) )	
 		bSizer5.Add( self.m_Write, 0, wx.ALL|wx.ALIGN_CENTER_HORIZONTAL, 5 )
 		
 		self.m_delete = wx.Button( sbSizer3.GetStaticBox(), wx.ID_ANY, \
                             u"Delete Last Record", wx.DefaultPosition, wx.Size( 140,30 ), 0 )
 		self.m_delete.SetMinSize( wx.Size( 130,30 ) )
-		self.m_delete.SetMaxSize( wx.Size( 160,40 ) )
-		
+		self.m_delete.SetMaxSize( wx.Size( 160,40 ) )		
 		bSizer5.Add( self.m_delete, 0, wx.ALL|wx.ALIGN_CENTER_HORIZONTAL, 5 )
-		
 		
 		bSizer4.Add( bSizer5, 1, wx.EXPAND, 5 )
 		
-		sbSizer2 = wx.StaticBoxSizer( wx.StaticBox( sbSizer3.GetStaticBox(), wx.ID_ANY, u"Current annotations" ), wx.VERTICAL )
+		sbSizer2 = wx.StaticBoxSizer( wx.StaticBox( sbSizer3.GetStaticBox(), wx.ID_ANY, \
+                                             u"Current annotations" ), wx.VERTICAL )
 		
-		self.AnnotationArea = wx.TextCtrl( sbSizer2.GetStaticBox(), wx.ID_ANY, wx.EmptyString,\
+		sbSizer2.SetMinSize( wx.Size( 210,-1 ) ) 
+		self.AnnotationArea = wx.TextCtrl( sbSizer2.GetStaticBox(), wx.ID_ANY, wx.EmptyString, \
                                     wx.DefaultPosition, wx.DefaultSize, wx.TE_MULTILINE|wx.TE_READONLY )
-		self.AnnotationArea.SetFont( wx.Font( wx.NORMAL_FONT.GetPointSize(), 70, 90, 90, False, "Calibri" ) )
-		
+		self.AnnotationArea.SetFont( wx.Font( wx.NORMAL_FONT.GetPointSize(), 70, 90, 90, False, "Calibri" ) )		
 		sbSizer2.Add( self.AnnotationArea, 1, wx.ALL|wx.EXPAND|wx.ALIGN_CENTER_HORIZONTAL, 5 )
-		
-		
-		bSizer4.Add( sbSizer2, 1, wx.EXPAND, 5 )
-		
-		
-		sbSizer3.Add( bSizer4, 1, wx.EXPAND, 5 )
-		
-		
-		bSizer1.Add( sbSizer3, 15, wx.EXPAND, 5 )
-		
+				
+		bSizer4.Add( sbSizer2, 1, wx.EXPAND, 5 )		
+		sbSizer3.Add( bSizer4, 1, wx.EXPAND, 5 )		
+		bSizer1.Add( sbSizer3, 15, wx.EXPAND, 5 )		
 		bSizer5 = wx.BoxSizer( wx.VERTICAL )
 		
+		bSizer5.SetMinSize( wx.Size( -1,50 ) ) 
 		self.m_slider = wx.Slider( self, wx.ID_ANY, 0, 0, 4000, wx.DefaultPosition, wx.Size( -1,40 ), wx.SL_LABELS|wx.SL_TOP )
 		self.m_slider.SetMinSize( wx.Size( -1,30 ) )
 		self.m_slider.SetMaxSize( wx.Size( -1,50 ) )
 		
-		bSizer5.Add( self.m_slider, 0, wx.ALL|wx.EXPAND, 5 )
+		bSizer5.Add( self.m_slider, 1, wx.ALL|wx.EXPAND, 5 )
 		
 		bSizer6 = wx.BoxSizer( wx.HORIZONTAL )
 		
-		self.m_buttonSlow = wx.Button( self, wx.ID_ANY, u"0.5X", wx.DefaultPosition, wx.Size( 180,-1 ), 0 )
-		bSizer6.Add( self.m_buttonSlow, 0, wx.ALL, 5 )
+		bSizer6.SetMinSize( wx.Size( -1,20 ) ) 
+		self.m_buttonSlow = wx.Button( self, wx.ID_ANY, u"0.5X", wx.DefaultPosition, wx.Size( -1,-1 ), 0 )
+		bSizer6.Add( self.m_buttonSlow, 1, wx.ALL, 5 )
 		
-		self.m_buttonBefore = wx.Button( self, wx.ID_ANY, u"Last Frame", wx.DefaultPosition, wx.Size( 180,-1 ), 0 )
-		bSizer6.Add( self.m_buttonBefore, 0, wx.ALL, 5 )
+		self.m_buttonBefore = wx.Button( self, wx.ID_ANY, u"Last Frame", wx.DefaultPosition, wx.Size( -1,-1 ), 0 )
+		bSizer6.Add( self.m_buttonBefore, 1, wx.ALL, 5 )
 		
-		self.m_toggleBtn = wx.ToggleButton( self, wx.ID_ANY, u"Pause", wx.DefaultPosition, wx.Size( 270,-1 ), 0 )
-		bSizer6.Add( self.m_toggleBtn, 0, wx.ALL, 5 )
+		self.m_toggleBtn = wx.ToggleButton( self, wx.ID_ANY, u"Pause", wx.DefaultPosition, wx.Size( -1,-1 ), 0 )
+		bSizer6.Add( self.m_toggleBtn, 2, wx.ALL, 5 )
 		
-		self.m_buttonNext = wx.Button( self, wx.ID_ANY, u"Next Frame", wx.DefaultPosition, wx.Size( 180,-1 ), 0 )
-		bSizer6.Add( self.m_buttonNext, 0, wx.ALL, 5 )
+		self.m_buttonNext = wx.Button( self, wx.ID_ANY, u"Next Frame", wx.DefaultPosition, wx.Size( -1,-1 ), 0 )
+		bSizer6.Add( self.m_buttonNext, 1, wx.ALL, 5 )
 		
-		self.m_buttonFast = wx.Button( self, wx.ID_ANY, u"2X", wx.DefaultPosition, wx.Size( 180,-1 ), 0 )
-		bSizer6.Add( self.m_buttonFast, 0, wx.ALL, 5 )
+		self.m_buttonFast = wx.Button( self, wx.ID_ANY, u"2X", wx.DefaultPosition, wx.Size( -1,-1 ), 0 )
+		bSizer6.Add( self.m_buttonFast, 1, wx.ALL, 5 )		
 		
-		
-		bSizer5.Add( bSizer6, 1, wx.EXPAND, 5 )
-		
-		
-		bSizer1.Add( bSizer5, 1, wx.EXPAND, 5 )
-		
+		bSizer5.Add( bSizer6, 1, wx.EXPAND, 5 )		
+		bSizer1.Add( bSizer5, 0, wx.EXPAND, 5 )		
 		
 		self.SetSizer( bSizer1 )
 		self.Layout()
@@ -191,8 +188,13 @@ class AnnotationTool2 ( wx.Frame ):
 		self.m_toggleBtn.Bind( wx.EVT_TOGGLEBUTTON, self.Play_Pause )
 		self.m_buttonNext.Bind( wx.EVT_BUTTON, self.NextFrame )
 		self.m_buttonFast.Bind( wx.EVT_BUTTON, self.OnFast )
-		self.Bind( wx.EVT_TIMER, self.OnTime, id=wx.ID_ANY )
-	
+		self.Bind( wx.EVT_TIMER, self.OnTime, id = wx.ID_ANY )
+        
+		self.m_toggleBtn.Bind( wx.EVT_KEY_DOWN, self.KeyboardEvent )
+		self.m_buttonNext.Bind( wx.EVT_KEY_DOWN, self.KeyboardEvent )
+		self.m_buttonBefore.Bind( wx.EVT_KEY_DOWN, self.KeyboardEvent )
+		self.Bind( wx.EVT_KEY_DOWN, self.KeyboardEvent )	        
+        
         #Set flags
 		self.PAUSE_FLAG = False
 		self.PROCESSING_FLAG = False
@@ -239,13 +241,13 @@ class AnnotationTool2 ( wx.Frame ):
 		    self.Inform_bar.SetValue('Please load the video!')
 		event.Skip()
 	
-    # Write an annotation to both file and display on annotation window
+    # Write an annotation to file and display it on annotation window
 	def SurgemeWrite( self, event ):
 		if self.PROCESSING_FLAG:
             # if the complete annotation is not created
 		    if len(self.OneRow) == 2 and self.IndexSurgeme:
 		        self.OneRow.append('S' + str(self.IndexSurgeme))
-		        self.OneRow.append('F' if self.IndexSF else 'S')  
+		        self.OneRow.append('False' if self.IndexSF else 'True')  
 		        for item in self.OneRow:
 		            self.OneAnnotation += (item + ' ')
 		        self.AnnotationList.append(self.OneAnnotation + '\n')
@@ -254,8 +256,9 @@ class AnnotationTool2 ( wx.Frame ):
                 # initilize the recording buffer
 		        self.OneRow = []
 		        self.OneAnnotation = ''
-            # else if the complete annotation is created by 'create annotation' function
+            # else if the complete annotation was created by 'create annotation' function
 		    elif len(self.OneRow) == 4:
+		        self.OneAnnotation = self.m_Annotext.GetLineText(0)
 		        self.AnnotationList.append(self.OneAnnotation + '\n')
 		        self.AnnotationArea.AppendText(self.AnnotationList[-1])
 		        self.MyFileWriting()  
@@ -360,7 +363,7 @@ class AnnotationTool2 ( wx.Frame ):
 	def OnSlow( self, event ):
 		if self.FrameTime <= 200:
 		    self.FrameTime = self.FrameTime * 2
-		    self.m_timer1.Start(self.FrameTime)
+		    if not self.PAUSE_FLAG: self.m_timer1.Start(self.FrameTime)
 		    self.Inform_bar.SetValue('Current fps:' + str(1000/self.FrameTime))
 		else:
 		    self.Inform_bar.SetValue('Minimum fps reached!')
@@ -370,7 +373,7 @@ class AnnotationTool2 ( wx.Frame ):
 	def OnFast( self, event ):
 		if self.FrameTime >= 10:
 		    self.FrameTime = self.FrameTime * 0.5
-		    self.m_timer1.Start(self.FrameTime)
+		    if not self.PAUSE_FLAG: self.m_timer1.Start(self.FrameTime)
 		    self.Inform_bar.SetValue('Current fps:' + str(1000/self.FrameTime))
 		else:
 		    self.Inform_bar.SetValue('Maximum fps reached!')
@@ -386,10 +389,21 @@ class AnnotationTool2 ( wx.Frame ):
 		else:
 		    event.Skip()
             
+	def KeyboardEvent( self, event ):
+		keycode = event.GetKeyCode()
+		if keycode in [wx.WXK_LEFT, wx.WXK_RIGHT] and self.PROCESSING_FLAG:
+		    self.FrameNumber = self.FrameNumber - 1 if keycode == wx.WXK_LEFT else self.FrameNumber + 1
+		    self.videoCapture.set(cv2.CAP_PROP_POS_FRAMES , self.FrameNumber)
+		    success, self.CurrentFrame = self.videoCapture.read()
+		    if(success):
+		        self.MyImshow()
+		event.Skip()
+            
     # Event handle defined, my functions below
-	def MyImshow(self, width = 640, height = 480):
+	def MyImshow(self):
+		self.CurrentFrame = cv2.resize(self.CurrentFrame, (self.ImWidth, self.ImHeight), interpolation = cv2.INTER_CUBIC)
 		image = cv2.cvtColor(self.CurrentFrame, cv2.COLOR_BGR2RGB)
-		pic = wx.Bitmap.FromBuffer(width, height, image) 
+		pic = wx.Bitmap.FromBuffer(self.ImWidth, self.ImHeight, image) 
 		self.m_bitmap.SetBitmap(pic)
 		self.m_slider.SetValue(int(self.FrameNumber))
         
@@ -406,11 +420,13 @@ class AnnotationTool2 ( wx.Frame ):
 
         
 if __name__ =='__main__':
-    video_path = r'D:\S\Project\data\temp.mp4'
+    # The size of display area and the path of video file, the annotation file will be in the same dictionary will the video file
+    VideoDisplaySize = [960, 540]
+    video_path = r'C:\Users\jyoth\OneDrive\Desktop\Forward\Data Collection\Taurus\data\S3\S3_T1_color.avi'
     fpath = video_path.split('.')
     write_path = fpath[0] + '_annot.txt'
     app = wx.App()
-    frame = AnnotationTool2(None, video_path, write_path)
+    frame = AnnotationTool2(None, video_path, write_path, VideoDisplaySize)
     frame.Show()
     app.MainLoop()
     del app
